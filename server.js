0c68b4d1223a62f2807ddffbf491127187726549
Almir Leandro <leandroalmm50@gmail.com>
2021-02-03 11:39:10 -0300

    Connect server to a SQL database

--- server.js
+++ server.js
@@ -1,63 +1,79 @@
+require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const bcrypt = require('bcryptjs');
+const knex = require('knex')({
+  client: 'pg',
+  connection: {
+    host : process.env.DB_HOST,
+    user : process.env.DB_USER,
+    password : process.env.DB_PASS,
+    database : process.env.DB_DATABASE
+  }
+});
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 
-const database = {
-  users: [
-    {
-      id: '123',
-      name: 'John',
-      email: 'john@gmail.com',
-      password: 'cookies',
-      joined: new Date()
-    },
-    {
-      id: '123',
-      name: 'Sally',
-      email: 'sally@gmail.com',
-      password: 'bananas',
-      joined: new Date()
-    }
-  ],
-  login: [
-    {
-      id: '987',
-      hash: '',
-      email: 'john@gmail.com'
-    }
-  ]
-}
-
 
 
 app.get('/', (req, res) => {
-  res.send(database.users);
+  res.send('Server is running');
 })
 
 app.post('/signin', (req, res) => {
-  if (req.body.email === database.users[0].email && req.body.password === database.users[0].password) {
-    res.json(database.users[0]);
-  } else {
-    res.status(400).json('error logging in');
-  }
+  knex.select('email', 'hash').from('login')
+    .where('email', '=', req.body.email)
+    .then(data => {
+      bcrypt.compare(req.body.password, data[0].hash, function(err, resp) {
+        if (resp) {
+          return knex.select('*').from('users')
+            .where('email', '=', req.body.email)
+            .then(user => {
+              res.json(user[0])
+            })
+            .catch(err => res.status(400).json('unable to get user'))
+        }
+        res.status(400).json('Wrong credentials')
+      })
+    })
+    .catch(err => res.status(400).json('Wrong credentials'))
 })
 
 app.post('/register', (req,res) => {
   const { email, name, password } = req.body;
-  database.users.push({
-    id: '125',
-    name: name,
-    email: email,
-    joined: new Date()
+  bcrypt.genSalt(10, function(err, salt) {
+    bcrypt.hash(password, salt, function(err, hash) {
+        // Store hash in your password DB.
+        knex.transaction(trx => {
+          trx.insert({
+            hash: hash,
+            email: email
+          })
+          .into('login')
+          .returning('email')
+          .then(loginEmail => {
+            return trx.insert({
+                name: name,
+                email: loginEmail[0],
+                joined: new Date()
+            })
+            .into('users')
+            .returning('*')
+            .then(user => {
+              res.json(user[0]);
+            })
+          })
+          .then(trx.commit)
+          .catch(trx.rollback)
+        })
+        .catch(err => res.status(400).json('Unable to register'))
+    });
   });
-  res.json(database.users[database.users.length-1]);
 })
 
 app.listen(3002, () => {
-  console.log('app is running at http://localhost:3002');
+  console.log('Server is running at http://localhost:3002');
 })
