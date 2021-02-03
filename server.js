3582f4d5858b8a5851f4cd33510cc9f160faff78
Almir Leandro <leandroalmm50@gmail.com>
2021-02-03 20:08:34 -0300

    Connect server to database

--- server.js
+++ server.js
@@ -7,10 +7,10 @@ const rateLimit = require("express-rate-limit");
 const knex = require('knex')({
   client: 'pg',
   connection: {
-    host : process.env.DB_HOST,
-    user : process.env.DB_USER,
-    password : process.env.DB_PASS,
-    database : process.env.DB_DATABASE
+    connectionString : process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false
+    }
   }
 });
 
